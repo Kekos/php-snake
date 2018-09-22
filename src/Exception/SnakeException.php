02708382d6e5b8fe1669c81bd808c3dd5c4f9d25
Christoffer Lindahl <christoffer@kekos.se>
2018-09-22 21:17:22 +0200

    SnakeException class ⚡️

---
+++ src/Exception/SnakeException.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/**
+ * PHP Snake
+ *
+ * Snake Exception
+ */
+
+namespace Kekos\PhpSnake\Exception;
+
+use Exception;
+
+class SnakeException extends Exception
+{
+}
