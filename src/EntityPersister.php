430c3447688cc5721d6adca26a31a0122d421ca3
Christoffer Lindahl <christoffer@kekos.se>
2018-10-05 17:34:34 +0200

    EntityPersister class.

---
+++ src/EntityPersister.php
@@ -0,0 +1,145 @@
+<?php declare(strict_types=1);
+/**
+ * PHP Snake
+ *
+ * EntityPersister class
+ */
+
+namespace Kekos\PhpSnake;
+
+use QueryBuilder\QueryBuilder;
+use QueryBuilder\QueryBuilders\Raw;
+
+final class EntityPersister
+{
+    /** @var EntityMeta */
+    private $meta;
+    /** @var Connection */
+    private $conn;
+    /** @var object[] */
+    private $queued_inserts = [];
+
+    public function __construct(EntityMeta $meta, Connection $conn)
+    {
+        $this->meta = $meta;
+        $this->conn = $conn;
+    }
+
+    public function addInsert(object $entity): void
+    {
+        $this->queued_inserts[spl_object_id($entity)] = $entity;
+    }
+
+    public function executeInserts(): array
+    {
+        if (empty($this->queued_inserts)) {
+            return [];
+        }
+
+        $insert_sql = $this->getInsertSql();
+        $primary_columns = $this->meta->getPrimaryKeyColumns();
+        $stmt = $this->conn->prepare($insert_sql);
+        $has_auto_increment = $this->meta->hasAutoIncrementPrimary();
+        $genereated_ids = [];
+
+        foreach ($this->queued_inserts as $entity) {
+            $values = $this->meta->getColumnsWithValues($entity);
+            $values = array_values(array_diff_key($values, $primary_columns));
+
+            $stmt->execute($values);
+
+            if ($has_auto_increment) {
+                $genereated_ids[spl_object_id($entity)] = $this->conn->getPdo()->lastInsertId();
+            }
+        }
+
+        $stmt->closeCursor();
+
+        return $genereated_ids;
+    }
+
+    public function update(object $entity): void
+    {
+        $update_sql = $this->getUpdateSql($entity);
+
+        $stmt = $this->conn->prepare((string) $update_sql);
+        $stmt->execute($update_sql->getParams());
+        $stmt->closeCursor();
+    }
+
+    public function delete(object $entity): void
+    {
+        $delete_sql = $this->getDeleteSql($entity);
+
+        $stmt = $this->conn->prepare((string) $delete_sql);
+        $stmt->execute($delete_sql->getParams());
+        $stmt->closeCursor();
+    }
+
+    public function getInsertSql(): string
+    {
+        $table_name = $this->meta->getTableName();
+        $qb = QueryBuilder::insert($table_name);
+        $primary_columns = $this->meta->getPrimaryKeyColumns();
+        $values = [];
+
+        foreach ($this->meta->getDefaultColumns() as $column) {
+            if (isset($primary_columns[$column])) {
+                continue;
+            }
+
+            $values[$column] = null;
+        }
+
+        $qb->values($values);
+
+        return (string) $qb->toSql();
+    }
+
+    public function getUpdateSql(object $entity): Raw
+    {
+        $table_name = $this->meta->getTableName();
+        $qb = QueryBuilder::update($table_name);
+        $primary_columns = $this->meta->getPrimaryKeyColumns();
+        $values = [];
+        $primary_values = [];
+
+        foreach ($this->meta->getColumnsWithValues($entity) as $column => $value) {
+            if (isset($primary_columns[$column])) {
+                $primary_values[$column] = $value;
+            } else {
+                $values[$column] = $value;
+            }
+        }
+
+        $qb->set($values);
+
+        foreach ($primary_values as $column => $value) {
+            $qb->where($column, '=', $value);
+        }
+
+        return $qb->toSql();
+    }
+
+    public function getDeleteSql(object $entity): Raw
+    {
+        $table_name = $this->meta->getTableName();
+        $qb = QueryBuilder::delete($table_name);
+        $primary_columns = $this->meta->getPrimaryKeyColumns();
+        $primary_values = [];
+
+        foreach ($this->meta->getColumnsWithValues($entity) as $column => $value) {
+            if (!isset($primary_columns[$column])) {
+                continue;
+            }
+
+            $primary_values[$column] = $value;
+        }
+
+        foreach ($primary_values as $column => $value) {
+            $qb->where($column, '=', $value);
+        }
+
+        return $qb->toSql();
+    }
+}
---
+++ tests/EntityPersisterTest.php
@@ -0,0 +1,189 @@
+<?php declare(strict_types=1);
+namespace Kekos\PhpSnake\Tests;
+
+use PDO;
+use ReflectionProperty;
+use Kekos\PhpSnake\Connection;
+use Kekos\PhpSnake\EntityMeta;
+use Kekos\PhpSnake\EntityPersister;
+use Kekos\PhpSnake\Tests\Fixtures\FooEntity;
+use PHPUnit\Framework\TestCase;
+use QueryBuilder\MySqlAdapter;
+use QueryBuilder\QueryBuilder;
+
+class EntityPersisterTest extends TestCase
+{
+    /** @var EntityPersister */
+    private $persister;
+    /** @var Connection */
+    private $conn;
+
+    public static function setUpBeforeClass(): void
+    {
+        QueryBuilder::setAdapter(new MySqlAdapter());
+    }
+
+    protected function setUp(): void
+    {
+        $this->conn = new Connection($_ENV['DB_DSN'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+        $this->persister = new EntityPersister(
+            new EntityMeta(FooEntity::class),
+            $this->conn
+        );
+
+        $this->conn->exec("CREATE TEMPORARY TABLE `foo_entity` (
+              `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+              `name` VARCHAR(64) NOT NULL,
+              `bar` VARCHAR(64) NULL,
+              `created_time` DATETIME NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
+    }
+
+    protected function tearDown(): void
+    {
+        $this->conn->exec("DROP TABLE `foo_entity`");
+    }
+
+    private function getTableCount(): int
+    {
+        $stmt = $this->conn->query("SELECT COUNT(*) AS `cnt` FROM `foo_entity`");
+
+        return $stmt->fetchObject()->cnt;
+    }
+
+    private static function getFilledEntity(): FooEntity
+    {
+        $entity = new FooEntity();
+        $entity
+            ->setName('name')
+            ->setBar(null)
+            ->setCreatedTime(date('Y-m-d'));
+
+        return $entity;
+    }
+
+    public function testAddInsert(): void
+    {
+        $entity = $this->getFilledEntity();
+        $oid = spl_object_id($entity);
+
+        $this->persister->addInsert($entity);
+
+        $this->assertAttributeEquals([
+            $oid => $entity,
+        ], 'queued_inserts', $this->persister);
+    }
+
+    public function testExecuteInserts(): void
+    {
+        $entity = $this->getFilledEntity();
+        $oid = spl_object_id($entity);
+
+        $this->persister->addInsert($entity);
+
+        $this->assertEquals(0, $this->getTableCount());
+
+        $generated_ids = $this->persister->executeInserts();
+
+        $this->assertEquals(1, $this->getTableCount());
+        $this->assertEquals([
+            $oid => 1,
+        ], $generated_ids);
+    }
+
+    public function testUpdate(): void
+    {
+        $entity = $this->getFilledEntity();
+
+        $this->persister->addInsert($entity);
+        $generated_ids = $this->persister->executeInserts();
+
+        $this->setEntityWithId($entity, intval(current($generated_ids)));
+
+        $expected = [
+            'name' => 'edit name',
+            'bar' => 'not null',
+        ];
+        $entity->setName($expected['name']);
+        $entity->setBar($expected['bar']);
+
+        $this->persister->update($entity);
+
+        $stmt = $this->conn->prepare("SELECT `name`, `bar` FROM `foo_entity` WHERE `id` = ?");
+        $stmt->execute([$entity->getId()]);
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDelete(): void
+    {
+        $entity = $this->getFilledEntity();
+
+        $this->persister->addInsert($entity);
+        $generated_ids = $this->persister->executeInserts();
+
+        $this->setEntityWithId($entity, intval(current($generated_ids)));
+
+        $this->assertEquals(1, $this->getTableCount());
+        $this->persister->delete($entity);
+        $this->assertEquals(0, $this->getTableCount());
+    }
+
+    private function setEntityWithId(object $entity, int $id): void
+    {
+        $reflection_prop = new ReflectionProperty(FooEntity::class, 'id');
+        $reflection_prop->setAccessible(true);
+        $reflection_prop->setValue($entity, $id);
+    }
+
+    public function testGetInsertSql(): void
+    {
+        $expected_sql = "INSERT INTO `foo_entity` (`name`, `bar`, `created_time`)\n\tVALUES (?, ?, ?)";
+
+        $sql = $this->persister->getInsertSql();
+
+        $this->assertEquals($expected_sql, (string) $sql);
+    }
+
+    public function testGetUpdateSql(): void
+    {
+        $entity = $this->getFilledEntity();
+        $this->setEntityWithId($entity, 42);
+
+        $expected_sql = "UPDATE `foo_entity`\n\t"
+            . "SET\n\t\t`name` = ?,\n\t\t`bar` = ?,\n\t\t`created_time` = ?\n\t"
+            . "WHERE `id` = ?";
+
+        $expected_params = [
+            $entity->getName(),
+            $entity->getBar(),
+            $entity->getCreatedTime(),
+            $entity->getId(),
+        ];
+
+        $sql = $this->persister->getUpdateSql($entity);
+
+        $this->assertEquals($expected_sql, (string) $sql);
+        $this->assertEquals($expected_params, $sql->getParams());
+    }
+
+    public function testGetDeleteSql(): void
+    {
+        $entity = new FooEntity();
+        $this->setEntityWithId($entity, 42);
+
+        $expected_sql = "DELETE FROM `foo_entity`\n\tWHERE `id` = ?";
+
+        $expected_params = [
+            $entity->getId(),
+        ];
+
+        $sql = $this->persister->getDeleteSql($entity);
+
+        $this->assertEquals($expected_sql, (string) $sql);
+        $this->assertEquals($expected_params, $sql->getParams());
+    }
+}
